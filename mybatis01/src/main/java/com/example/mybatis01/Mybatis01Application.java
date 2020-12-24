990e4b3df36e1b40ee92d6eb02264549693b5094
‘wul’ <279359180@qq.com>
2020-12-24 20:55:01 +0800

    #001 首次提交

---
+++ mybatis01/src/main/java/com/example/mybatis01/Mybatis01Application.java
@@ -0,0 +1,9 @@
+package com.example.mybatis01;
+
+public class Mybatis01Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ mybatis01/src/test/java/com/example/mybatis01/Mybatis01ApplicationTests.java
@@ -0,0 +1,5 @@
+package com.example.mybatis01;
+
+class Mybatis01ApplicationTests {
+
+}
